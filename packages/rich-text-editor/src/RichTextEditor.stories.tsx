3a198d3ceb82fcc1c4a39d61a828436d2d90d59f
Brian Kim <bryo.kim1@gmail.com>
2024-08-31 13:17:23 +0300

    fix: update import declaration for the RichTextEditor

--- packages/rich-text-editor/src/RichTextEditor.stories.tsx
+++ packages/rich-text-editor/src/RichTextEditor.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 
-import RichTextEditor from "./RichTextEditor";
+import { RichTextEditor } from "./RichTextEditor";
 import "./RichTextEditor.css";
 
 const meta = {
