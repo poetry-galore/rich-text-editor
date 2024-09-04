ef3927c4277e0c15b919695f73a2ed0721c5a434
Brian Kim <bryo.kim1@gmail.com>
2024-09-04 21:31:53 +0300

    version 0.0.19 release

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,52 +1,7 @@
-import * as react_jsx_runtime from "react/jsx-runtime";
 import { InitialEditorStateType } from "@lexical/react/LexicalComposer";
 import * as lexical from "lexical";
-import { LexicalEditor, EditorState } from "lexical";
-
-export declare class CustomEditorState {
-  /** The lexical editor */
-  editor: LexicalEditor;
-  /** Editor State */
-  editorState: EditorState;
-  constructor(editor: LexicalEditor, editorState: EditorState);
-  /**
-   * Get the JSON value of the editor
-   *
-   * @returns JSON value of the editor state.
-   */
-  toJSON(): lexical.SerializedEditorState<lexical.SerializedLexicalNode>;
-  /**
-   * Get the HTML value of the editor
-   *
-   * @returns HTML string of the editor state.
-   */
-  toHTML(): string;
-  /**
-   * Check if the editor is empty. An editor with whitespace only
-   * is taken to be empty.
-   *
-   * @returns boolean
-   */
-  isEmpty(): boolean;
-}
-
-export interface CustomOnChangePluginProps {
-  /**
-   * Callback triggered when the editor undergoes an update.
-   *
-   * @param customEditorState Custom Editor state
-   * @param tags
-   */
-  onChange: (customEditorState: CustomEditorState, tags?: Set<string>) => void;
-  /**
-   * Whether to ignore the selection change
-   */
-  ignoreSelectionChange?: boolean;
-  /**
-   * Whether to ignore the history merge change
-   */
-  ignoreHistoryMergeTagChange?: boolean;
-}
+import { EditorState, LexicalEditor } from "lexical";
+import * as react_jsx_runtime from "react/jsx-runtime";
 
 export declare const TEXT_ACTIONS: readonly [
   "bold",
@@ -143,11 +98,264 @@ export type PluginsConfig = {
 };
 
 /**
+ * Keys in the PluginsConfig type.
+ * Represents the available plugins.
+ */
+export type PluginsConfigKeys = keyof PluginsConfig;
+
+/**
  * Complete schema of the editor configuration.
  */
 export type EditorConfigSchema = {
   plugins?: PluginsConfig;
 };
+
+/**
+ * Types of configs used.
+ */
+export type AvailableConfigs = "default" | "user" | "merged";
+
+/**
+ * Paths to different parts of the configuration
+ */
+export type ConfigPaths =
+  | ""
+  | "plugins"
+  | "plugins.html"
+  | "plugins.html.register"
+  | "plugins.toolbar"
+  | "plugins.toolbar.blockTypes"
+  | "plugins.toolbar.historyActions"
+  | "plugins.toolbar.register"
+  | "plugins.toolbar.textActions"
+  | "plugins.floatingMenu"
+  | "plugins.floatingMenu.blockTypes"
+  | "plugins.floatingMenu.historyActions"
+  | "plugins.floatingMenu.register"
+  | "plugins.floatingMenu.textActions";
+/**
+ * Config with a name to be mapped to.
+ */
+export type NamedConfig = {
+  /**
+   * Name to map the config to.
+   * Cannot be an empty string.
+   */
+  name: string;
+  /**
+   * Configuration to apply to the editor.
+   * Will be mapped to name given.
+   */
+  config: EditorConfigSchema;
+};
+
+export declare class UserConfigMap extends Map<string, EditorConfigSchema> {}
+export declare class MergedConfigMap extends Map<string, EditorConfigSchema> {}
+
+/**
+ * Class for handling editor configuration
+ */
+export declare class Config {
+  /**
+   * Default configuration
+   *
+   * @private
+   */
+  _defaultConfig: EditorConfigSchema;
+  /**
+   * Mapping of the configurations provided by the user
+   *
+   * @private
+   */
+  _userConfigs: UserConfigMap;
+  /**
+   * Mapping of the userConfig merged with the defaultConfig.
+   * Configs are assigned same name as in userConfig.
+   *
+   * @see Config1.mergeConfigs
+   *
+   * @private
+   */
+  _mergedConfigs: MergedConfigMap;
+
+  constructor(config?: NamedConfig | NamedConfig[]);
+
+  get defaultConfig(): EditorConfigSchema;
+
+  /**
+   * @returns a clone of the _userConfigs
+   */
+  get userConfigs(): UserConfigMap;
+
+  /**
+   * @returns a clone of the _mergedConfigs
+   */
+  get mergedConfigs(): MergedConfigMap;
+
+  /**
+   * Gets the userConfig with the given name.
+   *
+   * @param name Name of the userConfig to get
+   *
+   * @returns userConfig with the given name if found, else undefined.
+   */
+  getUserConfigByName(name: string): EditorConfigSchema | undefined;
+
+  /**
+   * Gets the mergedConfig with the given name.
+   * Names for the mergedConfigs are same as those for the userConfigs.
+   *
+   * If the mergedConfig is not found, and userConfig with same name is found,
+   * the {@link Config1.mergeConfigs} method is called and the resulting mergedConfig
+   * is returned.
+   *
+   * @param name Name of the mergedConfig to get
+   *
+   * @returns mergedConfig with the given name if found, else undefined.
+   */
+  getMergedConfigByName(name: string): EditorConfigSchema | undefined;
+
+  /**
+   * Sets the userConfig with the given name to the given config.
+   * If a userConfig with the given name is not found, it is created.
+   * Also the mergedConfig is generated from the new userConfig.
+   *
+   * @param name Name to map to the config
+   * @param config Configuration passed to be mapped to the given name
+   */
+  setUserConfig(name: string, config: EditorConfigSchema): void;
+
+  /**
+   * Merges the `defaultConfig` and the `userConfig` into `mergedConfig`.
+   *
+   * @param name Name of the config to merge
+   *
+   * @see mergeConfigs
+   */
+  mergeConfigs(name: string): void;
+
+  /**
+   * Gets the configuration of the provided configType.
+   * Defaults to getting the `merged` configuration if the configType
+   * is not given.
+   *
+   * @param name Name mapped to the config to get. Must be provided for user and merged
+   * configTypes.
+   * @param configType The configuration to use. Either `user` or `merged`.
+   * Defaults to `merged`
+   *
+   * @returns Configuration of the type provided with the given name.
+   *
+   * @throws If the name is not a string.
+   * @throws If the configType is not either `merged` or `user`.
+   */
+  getConfigByName(
+    name: string,
+    configType?: Exclude<AvailableConfigs, "default">,
+  ): EditorConfigSchema | undefined;
+
+  /**
+   * Gets the configuration for a given configPath.
+   *
+   * @see ConfigPaths
+   *
+   * @param configPath The path of the configuration to get. eg `plugins.toolbar`.
+   * Defaults to "".
+   * @param name Name mapped to the config to use. This is required if the configType is
+   * `merged` or `user`.
+   * @param configType The configuration to use. Either `default`, `user` or `merged`.
+   * Defaults to `default`
+   *
+   * @returns Configuration matching the path, else undefined.
+   */
+  getConfigForPath(
+    configPath?: ConfigPaths,
+    name?: string,
+    configType?: AvailableConfigs,
+  ): any;
+
+  /**
+   * Checks whether a given plugin is registered.
+   *
+   * @param plugin Plugin to check if it's registered
+   * @param name Name mapped to the config to use. If given, then configType will be
+   * `merged` or `user`. `default` is not used when name is given.
+   * @param configType The configuration to use. Either `default`, `user` or `merged`.
+   * Defaults to `default`
+   *
+   * @returns If plugin is not configured, `false` is returned else the value of `register`
+   * for the given plugin is returned.
+   */
+  pluginIsRegistered(
+    plugin: PluginsConfigKeys,
+    name?: string,
+    configType?: AvailableConfigs,
+  ): any;
+}
+
+/**
+ * Define configuration to be used by the editor.
+ *
+ * @param editorConfig A single or array of configs that are going to be applied
+ * to the editor.
+ * @param configInstance Instance of the Config on which to define the configuration
+ * in. {@link EditorConfig} will be used if this param is not provided.
+ *
+ * @see {@link NamedConfig}
+ */
+export declare function defineConfig(
+  editorConfig: NamedConfig | NamedConfig[],
+  configInstance?: Config,
+): void;
+
+export declare class CustomEditorState {
+  /** The lexical editor */
+  editor: LexicalEditor;
+  /** Editor State */
+  editorState: EditorState;
+
+  constructor(editor: LexicalEditor, editorState: EditorState);
+
+  /**
+   * Get the JSON value of the editor
+   *
+   * @returns JSON value of the editor state.
+   */
+  toJSON(): lexical.SerializedEditorState<lexical.SerializedLexicalNode>;
+
+  /**
+   * Get the HTML value of the editor
+   *
+   * @returns HTML string of the editor state.
+   */
+  toHTML(): string;
+
+  /**
+   * Check if the editor is empty. An editor with whitespace only
+   * is taken to be empty.
+   *
+   * @returns boolean
+   */
+  isEmpty(): boolean;
+}
+
+export interface CustomOnChangePluginProps {
+  /**
+   * Callback triggered when the editor undergoes an update.
+   *
+   * @param customEditorState Custom Editor state
+   * @param tags
+   */
+  onChange: (customEditorState: CustomEditorState, tags?: Set<string>) => void;
+  /**
+   * Whether to ignore the selection change
+   */
+  ignoreSelectionChange?: boolean;
+  /**
+   * Whether to ignore the history merge change
+   */
+  ignoreHistoryMergeTagChange?: boolean;
+}
 
 export interface Props {
   /**
@@ -193,23 +401,29 @@ export interface Props {
    */
   onEditorChange?: CustomOnChangePluginProps[];
   /**
-   * Configuration of the editor.
+   * An instance of {@link Config} used in defineConfig.
+   * Set this to the configInstance passed in defineConfig if a custom one
+   * was used.
    */
-  editorConfig?: EditorConfigSchema;
+  configInstance?: Config;
+  /**
+   * Name of the configuraion to use.
+   * Should be one of the names of configurations provided in the defineConfig function
+   * if provided.
+   *
+   * If not given, then the default configuration is used.
+   */
+  configName?: string;
 }
 
 export type RichTextEditorProps = React.PropsWithChildren<Props> &
   React.AllHTMLAttributes<HTMLDivElement>;
 
 /**
- * Rich text editor built using Lexical.
+ * {@link RTE} wrapped in the {@link ConfigContextProvider}.
+ *
+ * Can be configured as needed using `defineConfig` function.
  */
-export declare function RichTextEditor({
-  placeholderText,
-  editable,
-  initialEditorState,
-  onEditorChange,
-  editorConfig,
-  children,
-  ...rest
-}: RichTextEditorProps): react_jsx_runtime.JSX.Element;
+export declare function RichTextEditor(
+  props: RichTextEditorProps,
+): react_jsx_runtime.JSX.Element;
